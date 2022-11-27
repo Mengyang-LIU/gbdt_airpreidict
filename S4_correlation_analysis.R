2672a476d86d5e9628bd26681a5966a8c6753fa4
Mengyang-LIU <58665544+Mengyang-LIU@users.noreply.github.com>
2022-11-27 17:27:15 +0800

    Update and rename corplot.R to S4_correlation_analysis.R

--- S4_correlation_analysis.R
+++ S4_correlation_analysis.R
@@ -4,7 +4,6 @@ corrplot(NA_AM1030)
 corrplot(cor(na.omit(agg_AM1103[,-1])), method="number", order="hclust", addrect=2, diag=F,tl.cex = 0.5,number.cex = 0.5)
 
 
-
 DF = cbind(all_PM %>% select(starts_with('backpm')),point_urban_info %>% select(starts_with('p15')|starts_with('p60')))
 DF = cbind(all_AM %>% select(starts_with('back')),point_urban_info[,-1])
 corrplot(cor(na.omit(all_df[-STA_ID,-c(1,34)])), method="color", order="original", addrect=2, diag=F,tl.cex = 0.5,number.cex = 0.3)
@@ -17,7 +16,6 @@ corrplot(cor(na.omit(cor_test2)), method="number", order="original", addrect=2, 
 b = as.data.frame(corrplot(cor(na.omit(DF)), method="number", order="original", addrect=2, diag=F,tl.cex = 1,number.cex = 0.3))
 
 a$buf_size = unlist(replicate(10, c(50,100,150,200), simplify = FALSE))
-
 
 agg_point$geometry[1]
 xy.coords(agg_point$geometry[1])
@@ -46,5 +44,3 @@ ggplot() +
   ggtitle("Urban Factor Correlation") +
   theme_light()
 
-
-#mh150
