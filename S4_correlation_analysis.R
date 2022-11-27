3a31655c11a140b3b2276578086bd8ee5a3ca7e8
Mengyang-LIU <58665544+Mengyang-LIU@users.noreply.github.com>
2022-11-27 17:27:49 +0800

    Update S4_correlation_analysis.R

--- S4_correlation_analysis.R
+++ S4_correlation_analysis.R
@@ -2,15 +2,12 @@ library(corrplot)
 library(sf)
 corrplot(NA_AM1030)
 corrplot(cor(na.omit(agg_AM1103[,-1])), method="number", order="hclust", addrect=2, diag=F,tl.cex = 0.5,number.cex = 0.5)
-
-
 DF = cbind(all_PM %>% select(starts_with('backpm')),point_urban_info %>% select(starts_with('p15')|starts_with('p60')))
 DF = cbind(all_AM %>% select(starts_with('back')),point_urban_info[,-1])
-corrplot(cor(na.omit(all_df[-STA_ID,-c(1,34)])), method="color", order="original", addrect=2, diag=F,tl.cex = 0.5,number.cex = 0.3)
 
+corrplot(cor(na.omit(all_df[-STA_ID,-c(1,34)])), method="color", order="original", addrect=2, diag=F,tl.cex = 0.5,number.cex = 0.3)
 corrplot(cor(na.omit(df_pm25[,-c(1,2,5,10,43)])), method="color", order="original", addrect=2, diag=F,tl.cex = 0.5,number.cex = 0.3)
 corrplot(cor(na.omit(all_df[-STA_ID,-c(1,34)])), method="number", order="original", addrect=2, diag=F,tl.cex = 1,number.cex = 0.3)
-
 corrplot(cor(na.omit(cor_test2)), method="number", order="original", addrect=2, diag=F,tl.cex = 1,number.cex = 0.3)
 
 b = as.data.frame(corrplot(cor(na.omit(DF)), method="number", order="original", addrect=2, diag=F,tl.cex = 1,number.cex = 0.3))
